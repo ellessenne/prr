f2dfb53b90ad54725fc7160e66a37d5417a7afe1
Alessandro Gasparini <ag475@le.ac.uk>
2017-07-31 14:59:54 +0100

    Added script for compiling the book in all formats

---
+++ make_book.R
@@ -0,0 +1,17 @@
+# This script compiles a bookdown book into a Gitbook, a .pdf, an .epub, and a .mobi file.
+
+# Clean-up session
+rm(list = ls()); gc()
+
+# Requires bookdown (duh)
+if (!require(bookdown)) install.packages(bookdown)
+
+# Clean up previous versions of the book
+bookdown::clean_book(T)
+
+# Compile all the formats
+bookdown::render_book("index.rmd", "bookdown::gitbook")
+bookdown::render_book("index.rmd", "bookdown::pdf_book")
+bookdown::render_book("index.rmd", "bookdown::epub_book")
+bookdown::kindlegen(epub = "docs/Probation_review_report_AG.epub") # This requires KindleGen (https://www.amazon.com/gp/feature.html?docId=1000765211); I have added the location of the KindleGen .exe to my PATH variable
+
