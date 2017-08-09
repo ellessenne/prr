6fccf2ced2ca3ecc92758d54b1a9c67b816ddec7
Alessandro Gasparini <ag475@le.ac.uk>
2017-08-09 10:32:52 +0100

    Added and wrote chapter on simulating survival data

--- make_book.R
+++ make_book.R
@@ -4,7 +4,7 @@
 rm(list = ls()); gc()
 
 # Requires bookdown (duh)
-if (!require(bookdown)) install.packages(bookdown)
+if (!require(bookdown)) install.packages("bookdown")
 
 # Clean up previous versions of the book
 bookdown::clean_book(T)
@@ -13,5 +13,5 @@ bookdown::clean_book(T)
 bookdown::render_book("index.Rmd", "bookdown::gitbook")
 bookdown::render_book("index.Rmd", "bookdown::pdf_book")
 bookdown::render_book("index.Rmd", "bookdown::epub_book")
-bookdown::kindlegen(epub = "docs/Probation_review_report_AG.epub") # This requires KindleGen (https://www.amazon.com/gp/feature.html?docId=1000765211), and its location has to be on the system's PATH
+bookdown::kindlegen(epub = "docs/Probation_review_report_AG.epub") # This requires KindleGen (https://www.amazon.com/gp/feature.html?docId=1000765211), and its location has to be on the system PATH
 
